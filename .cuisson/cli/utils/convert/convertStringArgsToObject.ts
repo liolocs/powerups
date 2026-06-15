47ae4eb38c38cd8114118024743ba3530d2441f2
Olivier Colas <olivercolas.oc@gmail.com>
2026-06-15 14:27:44 +0200

    chore: remove typescript version of cli

--- .cuisson/cli/utils/convert/convertStringArgsToObject.ts
+++
@@ -1,8 +0,0 @@
-export default function convertStringArgsToObject(args: string[]) {
-  // args expected to be in the form of ["--key1", "value1", "--key2", "value2"]
-  const result: Record<string, string> = {};
-  for (let i = 0; i < args.length; i += 2) {
-    result[args[i].replace(/^--/, "")] = args[i + 1];
-  }
-  return result;
-}
--- .cuisson/cli/utils/filesystem/importFile.ts
+++
@@ -1,5 +0,0 @@
-import fs from "@rcompat/fs";
-
-export default async function importFile(pathToFile: string) {
-  return await fs.ref(pathToFile).import()
-}

58cb24215824c72f331c563227a858009423cceb
Olivier Colas <olivercolas.oc@gmail.com>
2026-08-21 13:20:35 +0200

    refactor: drop removed commands from the command registry

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -1,27 +1,13 @@
 import { type Command } from "@liolocs/program";
-import add from "./add.js";
 import build from "./build.js";
 import create from "./create.js";
-import find from "./find.js";
 import install from "./install.js";
-import list from "./list.js";
-import metrics from "./metrics.js";
-import pack from "./pack.js";
-import project from "./project.js";
-import update from "./update.js";
 import use from "./use.js";
 
 const commands: Command<any>[] = [
-  add,
   build,
   create,
-  find,
   install,
-  list,
-  metrics,
-  pack,
-  project,
-  update,
   use,
 ];
 export default commands;

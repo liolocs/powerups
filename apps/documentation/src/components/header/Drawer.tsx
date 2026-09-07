c89e32ae4eb8ea363283797401f74457ea7e17a1
Olivier Colas <olivercolas.oc@gmail.com>
2026-09-07 12:03:02 +0200

    fix: nested button

--- apps/documentation/src/components/header/Drawer.tsx
+++ apps/documentation/src/components/header/Drawer.tsx
@@ -27,9 +27,9 @@ export default function HeaderDrawer({ topLinks = [], sectionLinks = [] }: Heade
   return (
     <Drawer showSwipeHandle>
       <DrawerTrigger>
-        <Button variant="ghost" size="icon">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="stroke-foreground size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5"></path></svg>
-        </Button>
+        <div className={buttonVariants({ variant: "ghost" })}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="stroke-foreground size-6"><path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5"></path></svg>
+        </div>
       </DrawerTrigger>
       <DrawerContent className="h-[70vh]">
         <div className="flex-1 p-4 flex flex-col gap-4">
